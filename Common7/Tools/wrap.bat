d4d51460ead21ba4a6a9e1a6b3f81882880be966
jdm7dv <jdm7dv@gmail.com>
2017-05-09 06:29:09 -0400

    restructured

--- Common7/Tools/wrap.bat
+++
@@ -1,5 +0,0 @@
-TITLE build.proj
-ECHO Off
-@set PATH=%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-msbuild.exe "C:\Users\Jonathan_Moore\Documents\Visual Studio 2010\Projects\C#\Singularity\base\MyFiles\Apps\TimingAlgorithm\build.proj"  
-pause
