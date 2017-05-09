682b62d4c3130f77e707b8102b57f6cc2b39d4dd
jdm7dv <jdm7dv@gmail.com>
2017-05-09 09:25:15 -0400

    added uml framework

---
+++ Common7/Tools/uml-framework/UMLToolingFramework/Diagrams/SequenceDiagram.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using TSF.UmlToolingFramework;
+
+namespace TSF.UmlToolingFramework.UML.Diagrams {
+  public interface SequenceDiagram:Diagram {
+
+  	
+	List<UML.Classes.Kernel.Operation> getCalledOperations();
+  }
+}
